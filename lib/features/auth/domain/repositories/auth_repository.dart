c313861858fb0deb4eace79538309fcd80897313
Tony Wang <Shir0o@users.noreply.github.com>
2025-12-10 13:38:06 -0800

    Add Google Sign-In support

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,9 +1,11 @@
 import '../entities/credentials.dart';
+import '../entities/google_account.dart';
 import '../entities/user.dart';
 
 abstract class AuthRepository {
   Future<User> login(Credentials credentials);
   Future<User> signup(Credentials credentials);
+  Future<User> loginWithGoogle(GoogleAccount account);
   Future<void> logout();
   Future<User?> currentUser();
 }
