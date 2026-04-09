77b8ffce5fce8387b77742d28996b81f262e34cc
Tony Wang <yilongwang05@gmail.com>
2026-04-09 14:14:52 -0700

    ci: fix integration test dependency conflicts by downgrading androidx.test libraries

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,10 +67,10 @@ android {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test:rules:1.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
 
 flutter {
