bb58d4e013cc9e7da1e10aa7b6306322293815dd
Tony Wang <yilongwang05@gmail.com>
2026-04-09 16:51:04 -0700

    ci: force modern androidx.test versions to fix android:exported manifest merger error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,12 +65,23 @@ android {
     }
 }
 
+configurations.all {
+    resolutionStrategy {
+        force("androidx.test:runner:1.5.2")
+        force("androidx.test:rules:1.5.0")
+        force("androidx.test:core:1.5.0")
+        force("androidx.test:monitor:1.6.1")
+        force("androidx.test.ext:junit:1.1.5")
+        force("androidx.test.espresso:espresso-core:3.5.1")
+    }
+}
+
 dependencies {
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test:rules:1.2.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 flutter {
